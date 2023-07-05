0cd17552d58e09a33411f967ccfb8e3ef63c149e
Gonzalo Salina <49573854+gesalina@users.noreply.github.com>
2023-07-05 08:47:04 -0300

    Update chat.router.js
    Nothing new, only comments

--- api/routers/chat/chat.router.js
+++ api/routers/chat/chat.router.js
@@ -4,10 +4,19 @@ import chatManager from "../../controllers/dao/ChatManager.js";
 const router = Router();
 const chat = new chatManager();
 
+/**
+* This endpoint show data about the chat
+* return user and message
+*/
 router.get("/", async (request, response) => {
     const getMessages = await chat.getMessages();
     response.json(getMessages);
 })
+/**
+* This endpoint insert data (user, message) on our messages collection
+* and emit a websocket to get on realtime the message write for the
+* users
+*/
 router.post("/",async (request, response) => {
     let data = request.body;
     const createMessage = await chat.insertMessage(data);
