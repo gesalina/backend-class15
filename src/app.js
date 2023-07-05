a1a7fb5806a49ae8fb36685048e5bce4489d297e
Gonzalo Salina <49573854+gesalina@users.noreply.github.com>
2023-07-05 11:56:55 -0300

    Update app.js
    Added comments

--- src/app.js
+++ src/app.js
@@ -11,28 +11,46 @@ import chatManager from "../api/controllers/dao/ChatManager.js";
 const app = express();
 const chat = new chatManager();
 
-// Templates engine config
+/**
+* Set the templates engine
+*/
 app.engine('handlebars', handlebars.engine());
 app.set('views', './api/routers/views');
 app.set('view engine', 'handlebars');
 
+/**
+* Middleware for parse to JSON 
+*/
 app.use(express.json())
+
+/**
+* Set the static folder
+*/
 app.use('/content', express.static('./public'));
 
-// Routers
-
+// API ROUTES
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/chat', chatRouter);
+
+// VIEW ROUTER
 app.use('/', viewRouter);
 
+/**
+* Establish database connection
+*/
 let serverHttp;
 
 try {
     await mongoose.connect('mongodb+srv://gonzaS:Gyr$$2310@cluster0.frrn8pq.mongodb.net/ecommerce')
     serverHttp = app.listen(8080, () => console.log(`Running server`));                                                      
-} catch(err) {console.log(err)}
-                                                   
+} catch(err) {
+    console.log(err)
+}
+
+/**
+* Establish websocket 
+*/
 const io = new Server(serverHttp);  
 
 app.set('socketio', io);
